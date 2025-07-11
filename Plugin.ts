3de040a1ec72b01f75d694fdc9cb845c36dbdee0
Stephen Renwick <stephen.renwick@omicronenergy.com>
2025-07-11 12:54:44 +0200

    feat: add plugin interface (shell constraint to APIs)

---
+++ Plugin.ts
@@ -0,0 +1,22 @@
+import { EditV2 } from './editv2.js';
+import { Transactor } from './Transactor.js';
+
+/**
+ * OpenSCD core communicates the data necessary for editing SCL documents by setting the
+ * following [properties](https://developer.mozilla.org/en-US/docs/Glossary/Property/JavaScript) on
+ * the plugin's [DOM Element](https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement)
+ *
+ * @property docs - A map of document names to their loaded XMLDocument instances.
+ * @property doc - The XMLDocument currently being edited, if any.
+ * @property docName - The name of the currently edited document, if any.
+ * @property docsState - changes value when the document is modified or documents are added/removed.
+ * @property locale - The end user's selected locale.
+ */
+export interface Plugin {
+  editor: Transactor<EditV2>;
+  docs: Record<string, XMLDocument>;
+  doc?: XMLDocument; // the document currently being edited
+  docName?: string; // the current doc's name
+  docsState: unknown; // current doc's state indicator
+  locale: string; // the end user's chosen locale
+}
